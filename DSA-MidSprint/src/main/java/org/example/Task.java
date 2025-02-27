c30d4ebf5c780f65bd76d3bb36f68e09db22418a
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-27 13:50:26 -0330

    Update Task.java
    Initialized taskDesc to set isComplete to false (task starts as incomplete by default)
    
    Added the following methods:
    -taskCompletion / marks tasks as completed
    -getTaskDesc / retrieves description of task
    -isComplete / checks for task completion
    -toString() / string representation of a task with completion status, for example: [Completed ✔️] or [Pending 💭]

--- DSA-MidSprint/src/main/java/org/example/Task.java
+++ DSA-MidSprint/src/main/java/org/example/Task.java
@@ -7,6 +7,29 @@ package org.example;
 
 // Task class stores the description & completion of a task, alongside a method marking tests as completed
 public class Task {
-    private String addTask; // use this to add a task
     private String taskDesc; // task description
+    private boolean isComplete; // tracks if a task is done
+
+    public Task(String taskDesc) {
+        this.taskDesc = taskDesc;
+        this.isComplete = false;
+    }
+
+    public void taskCompletion() { // method to mark task as completed
+        isComplete = true;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc; // used for external purpoes outside of Task class
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    @Override
+    public String toString() { // customizing how the Task object will be represented when converted to string format
+        return taskDesc + " [" + (isComplete ? "Completed ✔\uFE0F" : "Pending \uD83D\uDCAD") + "]";
+    } // the expected output: "Finish homework [Completed ✔️]", or "Finish homework [Pending 💭]"
 }
+
