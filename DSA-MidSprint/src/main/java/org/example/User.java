83d419bd7f3d6395f37f5891848904a2c413a0ff
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-27 16:27:20 -0330

    Update User.java
    Added TaskList, which represents users to-do list (tasks)
    
    The addTask, completeTask and viewTask methods interact with the tasklist object to interact with user given tasks

--- DSA-MidSprint/src/main/java/org/example/User.java
+++ DSA-MidSprint/src/main/java/org/example/User.java
@@ -8,13 +8,29 @@ package org.example;
 // An extension of Main, find User-related data here!
 public class User {
     private String name;
+    private TaskList tasklist; // represents user to-do list
 
     public User(String name) {
         this.name = name;
+        this.tasklist = new TaskList(); // initialize as a new TaskList object
     }
 
     public String getName() {
         return name;
+    }
+
+    /* ================= */
+    public void addTask(String taskDesc) {
+        task.ist.addTask(taskDesc);
+    }
+
+    public boolean completeTask(String taskDesc) {
+        return tasklist.markTaskCompleted(taskDesc);
+    }
+
+    public void viewTasks() {
+        System.out.println(name + "'s list of tasks:");
+        tasklist.printTasks(); // call the TaskList's method to print all tasks
     }
 }
 
